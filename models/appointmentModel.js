599a10d1944e8d1b75519ec46064eed599491976
ad0maa <adam.tunchay@me.com>
2023-01-30 16:32:06 +1100

    models adjusted

--- models/appointmentModel.js
+++ models/appointmentModel.js
@@ -1,36 +1,38 @@
 import mongoose from "mongoose";
 
-const appointmentSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-  },
-  doctorId: {
-    type: String,
-    required: true,
-  },
-  doctorInfo: {
-    type: Object,
-    required: false, // temp
-  },
-  userInfo: {
-    type: Object,
-    required: false, // temp
-  },
-  date: {
-    type: String,
-    required: true,
-  },
-  time: {
-    type: String,
-    required: true,
-  },
-  // bookingStatus: {
-  //   type: String,
-  //   required: true,
-  //   default: "pending",
-  // },
-});
+const appointmentSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    doctorId: {
+      type: String,
+      required: true,
+    },
+    doctorInfo: {
+      type: Object,
+      required: false, // temp
+    },
+    userInfo: {
+      type: Object,
+      required: false, // temp
+    },
+    date: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    // bookingStatus: {
+    //   type: String,
+    //   required: true,
+    //   default: "pending",
+    // },
+  }
+);
 
 const appointmentModel = mongoose.model("appointments", appointmentSchema);
 
